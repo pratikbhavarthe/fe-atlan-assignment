0f139ebf4e3577e1db6759f0d4e068dcabe546d5
Pratik Bhavarthe <pratikbhavarthee1998@gmail.com>
2024-02-19 02:37:46 +0530

    update packages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,41 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
-import Home from "./Pages/Home";
-import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
-import Favorite from "./Pages/Favorite";
-import TopModel from "./Pages/TopModel";
-import Explore from "./Pages/Explore";
-import ModelPage from "./Pages/ModelPage";
-import UploadModel from "./Pages/UploadModel";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-const router = createBrowserRouter([
-	{ path: "/", element: <Home /> },
-	{ path: "/Explore", element: <Explore /> },
-	{ path: "/Favorite", element: <Favorite /> },
-	{ path: "/Top-Model", element: <TopModel /> },
-	{ path: "/Models/:modelId", element: <ModelPage /> },
-	{ path: "/UploadModel", element: <UploadModel /> },
-]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
-		<div>
-			<Toaster
-				position="top-right"
-				toastOptions={{
-					success: {
-						theme: {
-							primary: "#4aed88",
-						},
-					},
-				}}
-			></Toaster>
-		</div>
-		<RouterProvider router={router} />
-	</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
